49552367662102cb485833970531ee9b09811351
Julian Orinyol <julianorinyol@gmail.comm>
2019-11-20 13:11:34 +0100

    feature: add basic server to return film data

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 8000
-
+const films = {
+	'herr_lehman': "bla",
+	'fakemovie': "blar"
+}
+const filmIds = Object.keys(films)
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/films', (req, res) => res.send(filmIds))
+
+app.get('/films/:filmId', (req, res) => {
+	const { filmId } = req.params
+	if(!(filmId in films)) {
+		return res.status(404).send(`There is no film with id ${filmId}`)
+	}
+
+	return res.send(films[filmId])
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
