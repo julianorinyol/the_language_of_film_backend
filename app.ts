d01836089d941bf61f99f19d745cc90349c34479
Julian Orinyol <julianorinyol@gmail.comm>
2019-11-21 13:26:21 +0100

    dependencies: add typescript, types, tsconfig file, and convert existing code to use type hinting

--- app.ts
+++ app.ts
@@ -1,16 +1,20 @@
+import { Router, Request, Response } from 'express';
+
 const express = require('express')
 const app = express()
 const port = 8000
-const films = {
+
+const films: { [key: string]: string } = {
 	'herr_lehman': "bla",
 	'fakemovie': "blar"
-}
+};
+
 const filmIds = Object.keys(films)
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
 
-app.get('/films', (req, res) => res.send(filmIds))
+app.get('/films', (req: Request, res: Response) => res.send(filmIds))
 
-app.get('/films/:filmId', (req, res) => {
+app.get('/films/:filmId', (req: Request, res: Response) => {
 	const { filmId } = req.params
 	if(!(filmId in films)) {
 		return res.status(404).send(`There is no film with id ${filmId}`)
