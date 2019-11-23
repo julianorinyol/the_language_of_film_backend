41f7c96f81e1e99d294dfc58fc88d5acd91bc5b3
Julian Orinyol <julianorinyol@gmail.comm>
2019-11-23 22:15:32 +0100

    dependencies: add cors and configure it for all routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import BaseRouter from './controllers';
+import cors from 'cors'
 
 const express = require('express')
 const app = express()
+app.use(cors())
+
 
 app.use('/api/v1/', BaseRouter);
 
