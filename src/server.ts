5b249051948c4f873b26b52152ed172b233411c0
Julian Orinyol <julianorinyol@gmail.comm>
2020-01-17 16:12:22 +0100

    chore: change databaseHelper to return promise

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import DatabaseHelper from './databaseHelper'
 const port: Number = Number(process.env.PORT)
 
 app.set("port", port);
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+DatabaseHelper.initializeDatabase().then(() => {
+	app.listen(port, () => console.log(`Example app listening on port ${port}!`))	
+})
 
-DatabaseHelper.initializeDatabase()
+
