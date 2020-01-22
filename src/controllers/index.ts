ad97df72bd026f99c3af0a87878b74a29dd49eb2
Julian Orinyol <julianorinyol@gmail.comm>
2020-01-22 15:20:24 +0100

    feature: add API controller for Cards for standardization

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import FilmRouter from './Films';
 import WordsRouter from './Words';
+import CardsRouter from './Cards';
 
 // Init router and path
 const router = Router();
@@ -8,6 +9,7 @@ const router = Router();
 // Add sub-routes
 router.use('/films', FilmRouter);
 router.use('/words', WordsRouter);
+router.use('/cards', CardsRouter);
 
 // Export the base-router
 export default router;
