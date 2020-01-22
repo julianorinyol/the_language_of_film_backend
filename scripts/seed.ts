23d0df246bcff2303147c4112bb14f4898b5a191
Julian Orinyol <julianorinyol@gmail.comm>
2020-01-22 14:19:21 +0100

    feature: update seed script to have some example translations

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,26 +1,56 @@
 
 import express = require('express')
+import mongoose from 'mongoose'
+
 
 import { Film, FilmData} from "../src/models/Film";
 import { Word, WordData } from '../src/models/Word';
+import { Translation, TranslationData } from '../src/models/Translation';
 import DatabaseHelper from '../src/databaseHelper'
 const defaultFilmImage = 'https://images.all-free-download.com/images/graphiclarge/movie_poster_background_art_vector_530172.jpg'
 
-const filmsData: FilmData[] = [
-    { name: 'movie 1', img: defaultFilmImage},
-    { name: 'movie 2', img: defaultFilmImage } 
+const filmsData: FilmData [] = [
+    {
+		name: 'movie 1',
+		img: defaultFilmImage
+	},
+    { 
+		name: 'movie 2',
+		img: defaultFilmImage 
+	} 
 ]
+
+
 
 const LANGUAGE_ENGLISH: 'english' = 'english'
 const LANGUAGE_GERMAN: 'german' = 'german'
+const LANGUAGE_SPANISH: 'spanish' = 'spanish'
 
 
+const word1Id = new mongoose.Types.ObjectId()
+const word2Id = new mongoose.Types.ObjectId()
+const word3Id = new mongoose.Types.ObjectId()
 
 const wordsData: WordData[] = [
-	{ word: 'verdammte', language: LANGUAGE_GERMAN },
-	{ word: 'damned', language: LANGUAGE_ENGLISH }
+	{ 
+		_id: word1Id,
+		word: 'verdammte', 
+		language: LANGUAGE_GERMAN,
+		translations: [ word2Id, word3Id ]
+	},
+	{ 
+		_id: word2Id,
+		word: 'damned', 
+		language: LANGUAGE_ENGLISH,
+		translations: [ word1Id, word3Id ]
+	},
+	{ 
+		_id: word3Id,
+		word: 'maldita', 
+		language: LANGUAGE_SPANISH,
+		translations: [ word1Id, word2Id ]
+	}
 ]
-
 const createItems = (Model: any, data:any, name: string) => {
 	return Promise.all(data.map(async (rawItem:any) => {
 	    return new Model(rawItem).save()
@@ -33,8 +63,6 @@ const createItems = (Model: any, data:any, name: string) => {
 		throw err
 	})
 }
-
-
 
 DatabaseHelper.initializeDatabase()
 .then(() => {
--- src/models/Word.ts
+++ src/models/Word.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 export type WordData = {
+    _id?: any;
     word: string;
     language: string;
+    translations?: any[];
 }
 
 export type WordDocument = mongoose.Document & WordData;
@@ -10,6 +12,7 @@ export type WordDocument = mongoose.Document & WordData;
 const wordSchema = new mongoose.Schema({
     word: { type: String },
     language: { type: String },
+    translations: [ {type : mongoose.Schema.Types.ObjectId, ref : 'Word'} ]
 }, { timestamps: true });
 
 export const Word = mongoose.model<WordDocument>("Word", wordSchema);
