8471e4cf9c8b8f74811aa829a6096d30a399aa4e
Julian Orinyol <julianorinyol@gmail.comm>
2020-01-29 16:16:27 +0100

    feature: add server status endpoint /ping, so front-end e2e test suite can check if backend is available

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,12 @@ const express = require('express')
 const app = express()
 app.use(cors())
 
+app.get('/ping', cors(), function (req: any, res: any) {
+	return res.send('pong');
+});
+
 const version = `v1`
 app.use(`/api/${version}/`, BaseRouter);
+
 
 export default app
