8f1bd93adb228ef7d14ca3c85e394eab88d284e6
Julian Orinyol <julianorinyol@gmail.comm>
2020-03-05 15:20:32 +0100

    feature: add passwordHelper and tests. Handles encryption and comparing to hash

---
+++ tests/helpers/passwordHelper.test.ts
@@ -0,0 +1,21 @@
+import bcrypt from 'bcryptjs'
+import { comparePassword } from '../../src/helpers/passwordHelper'
+
+describe(`comparePassword`, () => {
+    it(`should return true if password is correct`, async () => {
+        const password = 'hunter9'
+        const salt = bcrypt.genSaltSync(10);
+        const expected = await bcrypt.hash(password, salt)
+        const isValid = await comparePassword(password, expected)
+        return expect(isValid).toBe(true)
+    })
+
+    it(`should return false if password is incorrect`, async () => {
+        const password = 'hunter9'
+        const password2 = 'hunter10'
+        const salt = bcrypt.genSaltSync(10);
+        const expected = await bcrypt.hash(password, salt)
+        const isValid = await comparePassword(password2, expected)
+        return expect(isValid).toBe(false)
+    })
+})
