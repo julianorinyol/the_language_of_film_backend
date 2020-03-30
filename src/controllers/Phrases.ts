762de427d5dc8b41cf028534cc2ddf0dc5c42dbd
Julian Orinyol <julianorinyol@gmail.comm>
2020-03-31 00:50:51 +0200

    feature: add_or_update_many endpoints for both words and phrases

--- src/controllers/Phrases.ts
+++ src/controllers/Phrases.ts
@@ -1,7 +1,8 @@
 
 import { Request, Response, Router } from 'express';
-import { OK, INTERNAL_SERVER_ERROR, NOT_FOUND } from 'http-status-codes';
+import { OK, BAD_REQUEST, INTERNAL_SERVER_ERROR, NOT_FOUND } from 'http-status-codes';
 import { Phrase, PhraseData } from "../models/Phrase";
+import { AuthController } from './Auth'
 
 const router = Router();
 
@@ -36,8 +37,62 @@ export const PhrasesController = {
                 return res.status(NOT_FOUND).json({error: `No phrase found with id ${phraseId}`})
             })
     },
+    add: (req: Request, res: Response) => {
+        try {
+            return new Phrase(req.body).save().then(phrase => {
+                return res.status(200).json(phrase)
+            })
+        } catch(err) {
+            console.error(`error creating phrase`, err)
+            return res.status(INTERNAL_SERVER_ERROR).json({
+                error: err.message,
+            });
+        }
+    },
+    addOrUpdateMany: async (req: Request, res: Response) => {
+        const phrases = req.body
+        if(phrases.constructor.name !== 'Array') {
+            return res.status(BAD_REQUEST).json({error: {message: "body should be array"}})        
+        }
+
+        const promises = phrases.map( async ( phraseData:any ) => {
+            const translationsIds = await Promise.all(phraseData.translations.map(async (translatedPhraseData:any) => {
+                const  { translations, ...withoutTranslation   } =translatedPhraseData
+
+                return Phrase.findOneAndUpdate({phrase: translatedPhraseData.phrase, language: translatedPhraseData.language}, { $set: withoutTranslation }, { upsert: true, new: true })
+                    .exec()
+                    .then((phrase:any) => {
+                        return phrase._id
+                    }) 
+            }))
+
+
+
+            const  { translations, ...phraseDataWithoutTranslations } = phraseData
+            return Phrase.findOneAndUpdate({phrase: phraseData.phrase, language: phraseData.language},{ $set: phraseDataWithoutTranslations, $addToSet: { translations: translationsIds  } }, { upsert: true, new: true  })
+            .exec()
+            .then(async (phrase:any) => {
+                const all = await Promise.all(phrase.translations.map(async (x:any) => { 
+                    return Phrase.findByIdAndUpdate({ _id: x },{ $addToSet: { translations: phrase._id } }).exec()
+                }))
+                return phrase
+            }) 
+        })
+
+        return Promise.all(promises).then((completed:any) => {
+            return res.status(200).json(completed)        
+        }).catch((err:any) => {
+            console.error(`error creating phrase`, err)
+            return res.status(INTERNAL_SERVER_ERROR).json({
+                error: err.message,
+            });
+        })
+    },
+
 }
 
+const authController = new AuthController()
 router.get('/', PhrasesController.find);
 router.get('/:phraseId', PhrasesController.get);
+router.post("/add_or_update_many", authController.authenticateJWT, PhrasesController.addOrUpdateMany)
 export default router;
--- src/controllers/Words.ts
+++ src/controllers/Words.ts
@@ -1,7 +1,8 @@
 
 import { Request, Response, Router } from 'express';
-import { OK, INTERNAL_SERVER_ERROR, NOT_FOUND } from 'http-status-codes';
+import { OK, BAD_REQUEST, INTERNAL_SERVER_ERROR, NOT_FOUND } from 'http-status-codes';
 import { Word, WordData } from "../models/Word";
+import { AuthController } from './Auth'
 
 const router = Router();
 
@@ -36,8 +37,62 @@ export const WordsController = {
                 return res.status(NOT_FOUND).json({error: `No word found with id ${wordId}`})
             })
     },
+    add: (req: Request, res: Response) => {
+        try {
+            return new Word(req.body).save().then(word => {
+                return res.status(200).json(word )
+            })
+        } catch(err) {
+            console.error(`error creating word`, err)
+            return res.status(INTERNAL_SERVER_ERROR).json({
+                error: err.message,
+            });
+        }
+    },
+    addOrUpdateMany: async (req: Request, res: Response) => {
+        const words = req.body
+        if(words.constructor.name !== 'Array') {
+            return res.status(BAD_REQUEST).json({error: {message: "body should be array"}})        
+        }
+
+        const promises = words.map( async ( wordData:any ) => {
+            const translationsIds = await Promise.all(wordData.translations.map(async (translatedWordData:any) => {
+                const  { translations, ...withoutTranslation   } =translatedWordData
+
+                return Word.findOneAndUpdate({word: translatedWordData.word, language: translatedWordData.language}, { $set: withoutTranslation }, { upsert: true, new: true })
+                    .exec()
+                    .then((word:any) => {
+                        return word._id
+                    }) 
+            }))
+
+
+
+            const  { translations, ...wordDataWithoutTranslations } = wordData
+            return Word.findOneAndUpdate({word: wordData.word, language: wordData.language},{ $set: wordDataWithoutTranslations, $addToSet: { translations: translationsIds  } }, { upsert: true, new: true  })
+            .exec()
+            .then(async (word:any) => {
+                const all = await Promise.all(word.translations.map(async (x:any) => { 
+                    return Word.findByIdAndUpdate({ _id: x },{ $addToSet: { translations: word._id } }).exec()
+                }))
+                return word
+            }) 
+        })
+
+        return Promise.all(promises).then((completed:any) => {
+            return res.status(200).json(completed)        
+        }).catch((err:any) => {
+            console.error(`error creating word`, err)
+            return res.status(INTERNAL_SERVER_ERROR).json({
+                error: err.message,
+            });
+        })
+    },
 }
+const authController = new AuthController();
 
 router.get('/', WordsController.find);
 router.get('/:wordId', WordsController.get);
+router.post("/", authController.authenticateJWT, WordsController.add)
+router.post("/add_or_update_many", authController.authenticateJWT, WordsController.addOrUpdateMany)
 export default router;
